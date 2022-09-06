37c6734b3ad39987288099ef0934193ea5ba1aa1
maciejwoj <maciek1woj@gmail.com>
2022-09-06 19:55:11 +0200

    inicjalizuje

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Navbar from "components/Navbar";
+import ResourceHighlight from "components/ResourceHighlight";
+import Newsletter from "components/Newsletter";
+import ResourceList from "components/ResourceList";
+import Footer from "components/Footer";
+
+
+
+
+function Home() {
+
+  return (
+    <>
+      <Navbar />
+      <ResourceHighlight />
+      <Newsletter />
+      <ResourceList />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
+
